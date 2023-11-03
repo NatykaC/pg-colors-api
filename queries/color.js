7f4af5fe09c4d347161740425fe01829a192f099
Natyka Callwood <Natykacallwood@pursuit.org>
2023-11-03 10:14:45 -0400

    Fully working backend with routes to get, show, create, update and delete.

--- queries/color.js
+++ queries/color.js
@@ -29,7 +29,34 @@ const createColor = async (color)=>{
     }
 }
 
+const deleteColor = async (id)=>{
+    try {
+        const deletedColor = await db.one("DELETE FROM colors WHERE id=$1 RETURNING *", id)
+        return deletedColor
+    } catch (error) {
+        return error
+    }
+}
+
+const updateColor = async (id, color)=>{
+    try {
+        const updatedColor = await db.one(
+            "UPDATE colors SET name=$1, is_favorite=$2 WHERE id=$3 RETURNING *",
+            [color.name, color.is_favorite, id]
+        )
+        return updatedColor
+    } catch (error) {
+        return error
+    }
+}
+
+
+
+
+
 module.exports = { 
     getAllColors, 
     getColor, 
-    createColor }
+    createColor,
+    deleteColor,
+    updateColor }
